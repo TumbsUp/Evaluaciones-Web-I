571e8d0046d4dfcac378d357cebfd40308b3224a
Sebastian Mata <sebastianmata87@gmail.com>
2023-04-13 21:45:03 -0600

    Tarea 3 JS

--- Tarea 3/js/tarea3.js
+++ Tarea 3/js/tarea3.js
@@ -1,5 +1,5 @@
-function impar(){
-    let numb = document.getElementById('impar').value;
+//Función para verificar que un número sea par o impar
+function impar(numb){
     if(numb % 2 === 0){
         alert('El número es Par')
         return true;
@@ -9,9 +9,9 @@ function impar(){
         return false;
     }
 }
-function mayusc(){
-    let chain = document.getElementById('mayus').value;
-    if (chain.toUpperCase() === cadena) {
+//Función para Indicar si la cadena de texto contiene mayusculas, minusculas o ambas?
+function mayusc(chain){
+    if (chain.toUpperCase() === chain) {
         alert("La cadena está formada solo por mayúsculas.");
     } else if (chain.toLowerCase() === chain) {
         alert("La cadena está formada solo por minúsculas.");
@@ -19,14 +19,17 @@ function mayusc(){
         alert("La cadena está formada por una mezcla de mayúsculas y minúsculas.");
     } 
 }
-function palindro(){
-    let pal = document.getElementById('Palindromo').value;
-    if (pal.length < 1) {
+//Función la cual indica si una palabra es palindroma o no
+function palindro(pal){
+    pal2 = pal.toLowerCase().replace(/[\W_]/g, "");
+    if (pal2.length < 1) {
+        alert('Es palindromo')
         return true;
     } else {
-        if (pal[0] == pal[pal.length - 1]) {
-        return palindro(pal.slice(1, -1));
+        if (pal2[0] == pal2[pal2.length - 1]) {
+        return palindro(pal2.slice(1, -1));
         } else {
+            alert('No es palindromo')
         return false;
         }
     }
